ccde7ae889c61f0efe1924af0668e1cfe8d4423b
BernardoKava <bernardo.kava.bk@outlook.com>
2019-05-02 23:48:26 +0100

    final

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
+    @cahbox_text = " The CashBox is a virtual recepticle of all cash withdrawn from herga bank accounts.  A Cashbox is
+created for each month."
+    @trans_text = "The transaction is the itemised expenses made against all withdrawn cash values.  Cash withdrawals are
+flagged in the database.  The flagged transactions are automatically summed and displayed as indicated balance on the
+CashBox. "
   end
 end
